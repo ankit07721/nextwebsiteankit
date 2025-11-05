e98f5f9378083d7c4d6e5f2a7e87c974ef08c4ea
Ankit Gyanwali <ankitgyanwali001.com>
2025-11-05 21:26:09 +0545

    change in package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+
+//   experimental: {
+//     appDir: true,
+//   },
+//   turbopack: {
+//     root: "./src", // tell Next.js that your "src" folder is the root
+//   },
+
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-
-  experimental: {
-    appDir: true,
-  },
-  turbopack: {
-    root: "./src", // tell Next.js that your "src" folder is the root
-  },
-
+  // REMOVED: experimental: { appDir: true, }
+  // REMOVED: turbopack: { root: "./src", }
 };
 
 export default nextConfig;
